ef9caa9b126c4b058c5134080e6781da1bfb26aa
Yasamin Alizadeh <itsYasaminAlizadeh@gmail.com>
2022-06-21 14:34:13 +0430

    feat: do the math based on occured operator

--- index.js
+++ index.js
@@ -1,3 +1,56 @@
+const doMath = (arr, operator) => {
+  const index = arr.findIndex((item) => item === operator);
+
+  switch (operator) {
+    case "+":
+      if (isSubmited && isNaN(arr[index + 1])) throw "err/1";
+      if (!arr[index - 1]) arr.splice(index, 1);
+      else arr.splice(index - 1, 3, arr[index - 1] + arr[index + 1]);
+      break;
+
+    case "-":
+      if (isSubmited && isNaN(arr[index + 1])) throw "err/1";
+      arr.splice(index, 2, "+", -1 * arr[index + 1]);
+      break;
+
+    case "*":
+      if (isSubmited && (isNaN(arr[index - 1]) || isNaN(arr[index + 1])))
+        throw "err/1";
+      arr.splice(index - 1, 3, arr[index - 1] * arr[index + 1]);
+      break;
+
+    case "/":
+      if (
+        isSubmited &&
+        (isNaN(arr[index - 1]) || isNaN(arr[index + 1] || arr[index + 1] === 0))
+      )
+        throw "err/1";
+      else arr.splice(index - 1, 3, arr[index - 1] / arr[index + 1]);
+      break;
+
+    case "^":
+      if (isSubmited && (isNaN(arr[index - 1]) || isNaN(arr[index + 1])))
+        throw "err/1";
+      arr.splice(index - 1, 3, Math.pow(arr[index - 1], arr[index + 1]));
+      break;
+
+    case "(":
+      if (isSubmited && !isNaN(arr[index - 1])) throw "err/1";
+
+      const temp = arr.slice(
+        arr.lastIndexOf("(") + 1,
+        arr.indexOf(")", arr.lastIndexOf("("))
+      );
+
+      if (temp.length)
+        arr.splice(arr.lastIndexOf("("), temp.length + 2, calculate(temp));
+      else arr.splice(arr.lastIndexOf("("), 2);
+      break;
+
+    default:
+      break;
+  }
+};
 
 const checkInput = (value) => {
   let inputArr = value.match(/[/\+\-\*\/\^\(\)/i]|[\d]+/g);
